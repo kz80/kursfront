6826b94726df38be3b1092f2d9f9d114e27c4201
kz80 <katarzyna.grucel@gmail.com>
2022-09-12 23:10:26 +0200

    javascript

---
+++ javascript/script.js
@@ -0,0 +1,27 @@
+console.log("ELO2");
+
+const x = 1;
+let y = 1;
+// x = 2;
+y=3
+console.log('y=',y, ',x=',x)
+
+const person ="John"
+// console.log(personName)
+
+//OBIEKTY
+const person = {
+    name: "John",
+    age: 38,
+    outfit : {
+        hat: "Chanel",
+        shirt: "Supreme",
+        boots: "Nike"
+    }
+ }
+
+
+
+
+
+console.log(person)
